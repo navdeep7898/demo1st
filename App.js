d10beee14627e4627bbba7e6b84caf67fff79ca6
navdeep7898 <77379539+navdeep7898@users.noreply.github.com>
2021-01-18 20:51:04 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,35 +1,56 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, FlatList } from 'react-native';
 
-import React from 'react';
-import { StyleSheet, Text, View ,Image,ImageBackground,TouchableOpacity,TextInput,Button} from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle }
+    ]);
+    setIsAddMode(false);
+  };
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter(goal => goal.id !== goalId);
+    });
+  };
+
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
+  };
+
   return (
-    
-    <View style={styles.container}>
-      <Text>here is my first app</Text>
-      <TextInput placeholder="Username" placeholderColor="#c4c3cb" styles={{borderColor:'black',borderWidth: 1,padding:10}}/>
-      <Button
-              
-              onPress={() => this.onLoginPress()}
-              title="Login"
-              
-            />
-             <Button
-              // buttonStyle={styles.LoginButton}
-              onPress={() => this.onFbLoginPress()}
-              title="Login with Facebook"
-              
-            />
+    <View style={styles.screen}>
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput
+        visible={isAddMode}
+        onAddGoal={addGoalHandler}
+        onCancel={cancelGoalAdditionHandler}
+      />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => (
+          <GoalItem
+            id={itemData.item.id}
+            onDelete={removeGoalHandler}
+            title={itemData.item.value}
+          />
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection:'row',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen: {
+    padding: 50
+  }
 });
